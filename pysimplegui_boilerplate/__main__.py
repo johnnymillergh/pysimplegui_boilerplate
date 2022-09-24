5dd0ec3da66dc429638c73883fe760731c9df3d7
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2022-09-25 00:31:36 +0800

    feat($demo): create PySimpleGUI demo

--- pysimplegui_boilerplate/__main__.py
+++ pysimplegui_boilerplate/__main__.py
@@ -1,7 +1,41 @@
-import PySimpleGUI as sg
+import atexit
+import os
+import platform
+import sys
+import time
+from pathlib import Path
+
 from loguru import logger
 
 from pysimplegui_boilerplate.common.common_function import get_module_name
+from pysimplegui_boilerplate.configuration.application_configuration import (
+    configure as configure_application,
+)
+from pysimplegui_boilerplate.configuration.application_configuration import setup_cfg
+from pysimplegui_boilerplate.configuration.event_bus_configuration import (
+    configure as configure_event_bus,
+)
+from pysimplegui_boilerplate.configuration.loguru_configuration import (
+    configure as configure_loguru,
+)
+from pysimplegui_boilerplate.configuration.thread_pool_configuration import (
+    cleanup as cleanup_thread_pool,
+)
+from pysimplegui_boilerplate.configuration.thread_pool_configuration import (
+    configure as configure_thread_pool,
+)
+from pysimplegui_boilerplate.demo.hello_pysimplegui_window import show
+from pysimplegui_boilerplate.message.email import __init__
+from pysimplegui_boilerplate.message.email import cleanup as email_cleanup
+from pysimplegui_boilerplate.repository.model.startup_log import StartupLog
+from pysimplegui_boilerplate.repository.startup_log_repository import (
+    retain_startup_log,
+    save,
+    update_latest,
+)
+from pysimplegui_boilerplate.repository.trace_log_repository import retain_trace_log
+
+__startup_time = time.perf_counter()
 
 
 def main() -> None:
@@ -9,33 +43,58 @@ def main() -> None:
     Main function.
     """
     logger.info(f"Current module: {get_module_name()}")
+    show()
 
-    sg.theme("TanBlue")
-    sg.set_options(scaling=2.0)
-    layout = [
-        [sg.Text("What's your name?")],
-        [sg.Input(key="-INPUT-")],
-        [sg.Text(size=(40, 1), key="-OUTPUT-TEXT-")],
-        [sg.Button("OK"), sg.Button("Quit")],
-    ]
-    # Create the window
-    window = sg.Window("Window Title", layout)
 
-    # Display and interact with the Window using an Event Loop
-    while True:
-        event, values = window.read()
-        # See if the user wants to quit or window was closed
-        if event == sg.WINDOW_CLOSED or event == "Quit":
-            logger.info("Window closed")
-            break
-        # Output a message to the window
-        window["-OUTPUT-TEXT-"].update(
-            f"Hello {values['-INPUT-']}! Thanks for trying PySimpleGUI"
-        )
+def on_startup():
+    logger.info(
+        f"Starting {get_module_name()} using Python {platform.python_version()} on "
+        f"{platform.node()} with PID {os.getpid()} ({Path(__file__).parent})"
+    )
 
-    # Finish up by removing from the screen
-    window.close()
+    # Configuration
+    configure_application()
+    configure_loguru()
+    configure_thread_pool()
+    configure_event_bus()
+
+    # Initialization
+    __init__()
+
+    # Saving startup log
+    # Cannot save startup log in parallel, because the ThreadPoolExecutor won"t be able to start another future
+    # once the MainThread will end very soon.
+    # executor.submit(save, StartupLog(command_line=" ".join(sys.argv))).add_done_callback(done_callback)
+    save(StartupLog(command_line=" ".join(sys.argv)))
+
+    elapsed = time.perf_counter() - __startup_time
+    logger.info(
+        f"Started {get_module_name()}@{setup_cfg['metadata']['version']} in {round(elapsed, 3)} seconds "
+        f"({round(elapsed * 1000, 2)} ms) "
+        f"cmd: {''.join(sys.argv)}"
+    )
+
+
+@atexit.register
+def finalize() -> None:
+    """
+    Register `finalize()` function to be executed upon normal program termination.
+    """
+    logger.warning(f"Stopping {get_module_name()}, releasing system resources")
+    # Retain logs, in case the size of the SQLite database will be increasing like crazy.
+    retain_startup_log()
+    retain_trace_log()
+    # Shutdown tread pool and other connections
+    cleanup_thread_pool()
+    email_cleanup()
+    update_latest()
+    __end_elapsed = time.perf_counter() - __startup_time
+    logger.info(
+        f"Stopped {get_module_name()}, running for {round(__end_elapsed, 3)} seconds "
+        f"({round(__end_elapsed * 1000, 2)} ms) in total"
+    )
 
 
 if __name__ == "__main__":
+    on_startup()
     main()
